b7ab7714927d2a5150b7f6c8bf6158f697acaf7b
liyone <lixzone_liyang@Hotmail.com>
2023-12-16 16:21:29 -0500

    issue with nest core

--- src/backend/NestJS/src/main.ts
+++ src/backend/NestJS/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  await app.init();
+  return app;
 }
-bootstrap();
+
+export const handler = serverless(bootstrap);
--- src/infra/cdk/lib/lambda-stack.ts
+++ src/infra/cdk/lib/lambda-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from '@aws-cdk/aws-apigateway';
+import path = require('path');
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,13 +9,22 @@ export class LambdaStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const helloLambda = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('../backend/dist'), // path to the compiled NestJS code
+      runtime: lambda.Runtime.NODEJS_16_X,
+      // code: lambda.Code.fromAsset('../../../src/backend/NestJS/dist'), // path to the compiled NestJS code
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../../backend/NestJS/dist')), // path to the compiled NestJS code
       handler: 'main.handler', // exported handler in your NestJS code
+      //handler: 'healthTest.handler', // exported handler in your NestJS code
     });
 
-    new apigateway.LambdaRestApi(this, 'Endpoint', {
-      handler: helloLambda,
+    const api = new apigateway.RestApi(this, 'Endpoint');
+
+    const healthResource = api.root.addResource('health');
+
+    healthResource.addMethod('GET', new apigateway.LambdaIntegration(helloLambda));
+
+    // Output the API Gateway URL to the console during deployment
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
     });
   }
 }
